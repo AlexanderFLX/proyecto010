53dd1849d1d13299ff32af6b4dca76dbdb838fb0
Alexander <lguerrero@flx.com.pe>
2019-06-30 00:36:48 -0500

    Clase 15 - Petición de un archivo JSON a un servidor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'proyecto010';
+  
+  private urlApi = 'https://jsonplaceholder.typicode.com/users';
+  private usuarios = null;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get(this.urlApi).subscribe(
+      respuesta => this.usuarios = respuesta,
+      error => console.log('Error: ' + error),
+      () => console.log('Se completó la petición :D')
+    );
+  }
+
 }
